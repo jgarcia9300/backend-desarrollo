1fb0dac58a75495640a6c587b8b023d854675539
jgarcia9300 <juan.m.garcia@correounivalle.edu.co>
2024-06-11 19:12:23 -0500

    deploy una vez mas

--- proyectoDS/settings.py
+++ proyectoDS/settings.py
@@ -146,3 +146,8 @@ LOGOUT_REDIRECT_URL = 'home'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
