c59e711a6f78854cf5362a9f64b6eedd52689026
jgarcia9300 <juan.m.garcia@correounivalle.edu.co>
2024-10-21 19:04:30 -0500

    modificando prueba

--- core/tests.py
+++ core/tests.py
@@ -121,7 +121,7 @@ class AsignarTareasModelTest(TestCase):
 # Verificar que la instancia haya sido eliminada
         self.assertFalse(asignarTareas.objects.filter(id=tarea_id).exists())
 
-#Verificando actualizacion por medio de test_update
+#Verificando actualizacion
     def test_update_informe(self):
         # Crear y luego actualizar una instancia de informes
         informe = informes.objects.create(
